4d9b6f88b862f31fd575077d971ad3305061dcf8
Charles Conroy <charlieconroy@protonmail.com>
2021-06-24 18:38:23 -0400

    chore: moved code here from trend/graphapi
    This code will be used by many different projects
    of which, may belong to different git repos.

---
+++ src/test/index.test.ts
@@ -0,0 +1,221 @@
+import nock from 'nock';
+
+import { FacebookGraphAPI, login } from '../index';
+
+beforeAll(function() {
+  nock.disableNetConnect();
+});
+
+describe('facebook login', function() {
+  describe('user access token', function() {
+    let scope: nock.Scope;
+    beforeEach(function () {
+      scope = nock('https://graph.facebook.com')
+        .get(/\/oauth\/access_token\?/)
+        .reply(200, { access_token: '', token_type: '', expires_in: 0 });
+    });
+
+    afterEach(function() {
+      nock.cleanAll();
+    });
+
+    test('should login the user with application info and user access token', async function() {
+      expect.assertions(2);
+      const result = await login({
+        grant_type: 'fb_exchange_token',
+        client_id: 'appId',
+        client_secret: 'appSecret',
+        fb_exchange_token: 'userAccessToken',
+      });
+      expect(result.ok).toEqual(true);
+      expect(scope.isDone()).toEqual(true);
+    });
+  });
+
+  describe('app access token', function() {
+    let scope: nock.Scope;
+    beforeEach(function () {
+      scope = nock('https://graph.facebook.com')
+        .get(/\/oauth\/access_token\?/)
+        .reply(200, { access_token: '', token_type: '' });
+    });
+
+    afterEach(function() {
+      nock.cleanAll();
+    });
+
+    test('should login the user with application info and user access token', async function() {
+      expect.assertions(2);
+      const result = await login({
+        grant_type: 'client_credentials',
+        client_id: 'appId',
+        client_secret: 'appSecret',
+      });
+      expect(result.ok).toEqual(true);
+      expect(scope.isDone()).toEqual(true);
+    });
+  });
+});
+
+describe('facebook graph', function() {
+  let api: FacebookGraphAPI;
+
+  beforeAll(function() {
+    api = new FacebookGraphAPI('secretAccessToken');
+  });
+
+  describe('successful call to get node feed, node = \'group\'', function () {
+    let scope: nock.Scope;
+    beforeEach(function () {
+      scope = nock('https://graph.facebook.com')
+        .defaultReplyHeaders({
+          'x-app-usage': JSON.stringify({call_count:0, total_time:0, total_cputime: 0}),
+        })
+        .get(/\/1\/feed\?/)
+        .reply(200, { data: [], paging: {} });
+    });
+
+    afterEach(function() {
+      nock.cleanAll();
+    });
+
+    test('should make a successfull call', async function() {
+      expect.assertions(2);
+      const result = await api.getNodeFeed('1', 'Group');
+      expect(result.ok).toEqual(true);
+      expect(scope.isDone()).toEqual(true);
+    });
+  });
+
+  describe('error call to get node feed, node = \'group\'', function () {
+    let scope: nock.Scope;
+    beforeEach(function () {
+      scope = nock('https://graph.facebook.com')
+        .get(/\/1\/feed\?/)
+        .reply(400, { error: {} });
+    });
+
+    afterEach(function() {
+      nock.cleanAll();
+    });
+
+    test('should make an error call', async function() {
+      expect.assertions(2);
+      const result = await api.getNodeFeed('1', 'Group');
+      expect(result.ok).toEqual(false);
+      expect(scope.isDone()).toEqual(true);
+    });
+  });
+
+  describe('get error when problem with underlying req for get node feed, node = \'group\'', function () {
+    beforeEach(function () {
+      nock('https://graph.facebook.com')
+        .get(/\/1\/feed\?/)
+        .replyWithError('error making request')
+    });
+
+    afterEach(function() {
+      nock.cleanAll();
+    });
+
+    test('should make an error call', async function() {
+      expect.assertions(1);
+      return expect(api.getNodeFeed('1', 'Group'))
+        .rejects
+        .toThrowError('error making request');
+    });
+  });
+
+  describe('successful call to get node comments', function () {
+    let scope: nock.Scope;
+    beforeEach(function () {
+      scope = nock('https://graph.facebook.com')
+        .defaultReplyHeaders({
+          'x-app-usage': JSON.stringify({call_count:0, total_time:0, total_cputime: 0}),
+        })
+        .get(/\/1\/comments\?/)
+        .reply(200, { data: [], paging: {} });
+    });
+
+    afterEach(function() {
+      nock.cleanAll();
+    });
+
+    test('should make a successfull call', async function() {
+      expect.assertions(2);
+      const result = await api.getNodeComments('1');
+      expect(result.ok).toEqual(true);
+      expect(scope.isDone()).toEqual(true);
+    });
+  });
+
+  describe('successful call to get next page of results', function () {
+    let scope: nock.Scope;
+    beforeEach(function () {
+      scope = nock('https://graph.facebook.com')
+        .defaultReplyHeaders({
+          'x-app-usage': JSON.stringify({call_count:0, total_time:0, total_cputime: 0}),
+        })
+        .get(/\/1\/feed\?/)
+        .reply(200, { data: [], paging: {} });
+    });
+
+    afterEach(function() {
+      nock.cleanAll();
+    });
+
+    test('should make a successfull call', async function() {
+      expect.assertions(2);
+      const url = 'https://graph.facebook.com/v10.0/1/feed?fields=id%2Ccreated_time%2Cupdated_time%2Cpermalink_url%2Cfull_picture%2Ctype%2Cname%2Cmessage%2Clink%2Cshares%2Cstory%2Creactions.fields(id%2Ctype).summary(true)%2Ccomments.limit(1).fields(id).summary(true).filter(stream)&limit=50&access_token=secretAccessToken';
+      const result = await api.getNextPage(url);
+      expect(result.ok).toEqual(true);
+      expect(scope.isDone()).toEqual(true);
+    });
+  });
+
+  describe('successful call to get user\'s groups', function () {
+    let scope: nock.Scope;
+    beforeEach(function () {
+      scope = nock('https://graph.facebook.com')
+        .defaultReplyHeaders({
+          'x-app-usage': JSON.stringify({call_count:0, total_time:0, total_cputime: 0}),
+        })
+        .get(/\/me\/groups\?/)
+        .reply(200, { data: [], paging: {} });
+    });
+
+    afterEach(function() {
+      nock.cleanAll();
+    });
+
+    test('should make a successfull call', async function() {
+      expect.assertions(2);
+      const result = await api.getUserGroups();
+      expect(result.ok).toEqual(true);
+      expect(scope.isDone()).toEqual(true);
+    });
+  });
+
+  describe('successful call to get groups app is installed for', function () {
+    let scope: nock.Scope;
+    beforeEach(function () {
+      scope = nock('https://graph.facebook.com')
+        .defaultReplyHeaders({
+          'x-app-usage': JSON.stringify({call_count:0, total_time:0, total_cputime: 0}),
+        })
+        .get(/\/appId\/app_installed_groups\?/)
+        .reply(200, { data: [], paging: {} });
+    });
+
+    afterEach(function() {
+      nock.cleanAll();
+    });
+
+    test('should make a successfull call', async function() {
+      expect.assertions(2);
+      const result = await api.getGroupsAppInstalledFor('appId');
+      expect(result.ok).toEqual(true);
+      expect(scope.isDone()).toEqual(true);
+    });
+  });
+});
---
+++ src/types.ts
@@ -0,0 +1,204 @@
+export type NodeType =
+  | 'Group'
+  | 'Post';
+
+/**
+ * retry errors:
+ * API Unknown (code = 1)
+ * API Service (code = 2)
+ * Temporarily blocked for policies violations (368)
+ *
+ * rate limit errors:
+ * API Too Many Calls (code = 4)
+ * API User Too Many Calls (code = 17)
+ * Application limit reached (code = 341)
+ *
+ * renew access token:
+ * Access token has expired (code = 190)
+ */
+
+ export type LoginParams =
+ | { // user access token
+     grant_type: 'fb_exchange_token',
+     client_id: string,
+     client_secret: string,
+     fb_exchange_token: string
+   }
+ | { // app access token
+     grant_type: 'client_credentials',
+     client_id: string,
+     client_secret: string
+   };
+
+export interface FacebookError {
+  error: {
+    message: string;
+    type: string;
+    code: number;
+    error_subcode?: number;
+    fbtrace_id: string;
+  };
+}
+
+export interface FacebookLimits {
+  call_count: number;
+  total_time: number;
+  total_cputime: number;
+};
+
+export interface FacebookLoginResult {
+  access_token: string;
+  token_type: string;
+  expires_in?: number;
+}
+
+export interface Group {
+  id: string;
+  archived: boolean;
+  description: string;
+  cover: {
+    id: string;
+    source: string;
+  };
+  icon: string;
+  created_time: string; // fmt: 2021-04-05T19:59:49+0000
+  email: string;
+  member_count: number;
+  member_request_count: number;
+  name: string;
+  picture: {
+    data: {
+      height: number;
+      width: number;
+      is_silhouette: boolean;
+      url: string;
+    };
+  };
+  privacy: string;
+  parent: any;
+  administrator: boolean;
+}
+
+export interface GroupAppInstalledFor {
+  id: string;
+  name: string;
+}
+
+export interface Reactions {
+  reactions_none: {
+    summary: { total_count: number };
+  };
+  reactions_like: {
+    summary: { total_count: number };
+  };
+  reactions_love: {
+    summary: { total_count: number };
+  };
+  reactions_wow: {
+    summary: { total_count: number };
+  };
+  reactions_haha: {
+    summary: { total_count: number };
+  };
+  reactions_sad: {
+    summary: { total_count: number };
+  };
+  reactions_angry: {
+    summary: { total_count: number };
+  };
+  reactions_thankful: {
+    summary: { total_count: number };
+  };
+  reactions_pride: {
+    summary: { total_count: number };
+  };
+  reactions_care: {
+    summary: { total_count: number };
+  };
+}
+
+export interface Post extends Reactions {
+  id: string;
+  created_time: string; // fmt: 2021-05-06T19:29:03+0000
+  updated_time: string;
+  permalink_url: string;
+  type: string;
+  full_picture?: string;
+  name?: string;
+  message?: string;
+  link?: string;
+  shares?: any;
+  story?: string;
+  comments?: {
+    data: Array<{id: string}>;
+    paging: any; // ignore, query comments in another route
+    summary: {
+      order: string;
+      total_count: number;
+      can_comment: boolean;
+    };
+  };
+}
+
+export interface Comment extends Reactions {
+  id: string;
+  created_time: string; // fmt: 2021-04-05T21:31:59+0000
+  message: string;
+  permalink_url: string;
+  attachment?: {
+    description: string;
+    media: {
+      image: {
+        height: number;
+        width: number;
+        src: string;
+      };
+    };
+    target: {
+      url: string;
+    };
+    title: string;
+    type: string;
+    url: string;
+  };
+  parent?: {
+    id: string;
+    created_time: string;
+  }
+}
+
+export interface FacebookGraphResult<A> {
+  data: A[];
+  paging?: {
+    cursors?: {
+      after: string;
+      before: string;
+    };
+    previous?: string;
+    next?: string;
+  };
+}
+
+export type FacebookResponse<A> =
+  | { ok: true, limits: FacebookLimits | null, message: A }
+  | { ok: false, message: FacebookError };
+
+export interface GroupInstallWebhook {
+  field: string;
+  value: {
+    group_id: string;
+    update_time: string;  // time of update
+    verb: string;         // type of action taken
+    actor_id: string;     // admin who installed/uninstalled app
+  };
+};
+
+export interface WebhookResponse<A> {
+  entry: Array<{
+    time: number;
+    changes: A[];
+    id: string;
+    uid: string;
+  }>;
+  object: string;
+};
